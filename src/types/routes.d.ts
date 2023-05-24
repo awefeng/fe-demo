2505545e055a6a8ec3d1fe2b7b8104e22a8df526
awefeng <awefeng@163.com>
2023-05-24 22:16:54 +0800

    feat: upgrade react router v6 and type custom route (#5)
    ok to upgrade, react-router-v6 version: 6.11.2

--- src/types/routes.d.ts
+++ src/types/routes.d.ts
@@ -1,13 +1,19 @@
-import type { RouteObject } from 'react-router-dom'
+import type { IndexRouteObject, NonIndexRouteObject } from 'react-router-dom'
 import { USER_ROLE_ENUM } from '@/constants/user'
 
-// 扩展Route定义
-export interface RouteProps extends RouteObject {
+interface CustomRouteFields {
   meta?: {
     auth?: boolean
     // roles和unRoles冲突的时候，冲突的部分以unRoles为准
     roles?: USER_ROLE_ENUM[] // 空数组代表没有谁可以访问
     unRoles?: USER_ROLE_ENUM[] // 空数组代表没有谁不可以访问
   }
-  children?: RouteProps[]
 }
+
+type AppIndexRouteObject = IndexRouteObject & CustomRouteFields
+type AppNonIndexRouteObject = Omit<NonIndexRouteObject, 'children'> &
+  CustomRouteFields & {
+    children?: (AppIndexRouteObject | AppNonIndexRouteObject)[]
+  }
+
+export type RouteProps = AppIndexRouteObject | AppNonIndexRouteObject
